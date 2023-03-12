0390a63eaa1fc9f0d4df26feed5258fbdb3998b4
Marcus Kicklighter <marcus.kicklighter@gmail.com>
2023-03-12 22:07:34 +0100

    Either I am delerious or I have solved it. My tests are ok

--- lab3/Node.h
+++ lab3/Node.h
@@ -478,15 +478,15 @@ public:
 			BB* jumpPath = (*currentBlock)->getTrue(); // hBlock in whileStmt
 			jBlock->setTrue(jumpPath);
 		}
-
-		/* Assign the true and false path for the current block */
-		(*currentBlock)->setTrue(tBlock); 
-		(*currentBlock)->setFalse(fBlock);
 		
 		/*Fill fBlock and tBlock by calling genIR on various children. Called by reference*/
 		string tName = children[1]->genIR(&tBlock, methods, BBnames, id, blockID); // generate true block
 		string fName = children[2]->genIR(&fBlock, methods, BBnames, id, blockID); // generate false block
 		
+		/* Assign the true and false path for the current block */
+		(*currentBlock)->setTrue(tBlock); 
+		(*currentBlock)->setFalse(fBlock);
+
 		// Add Tacs, unconditional and conditional jumps
 		/*Split is conditional*/
 		CondTac* splitTac = new CondTac(conName, fBlock->getName()); // default is to go to true
--- lab3/test_files/valid/MarcusIndiv.java
+++ lab3/test_files/valid/MarcusIndiv.java
@@ -24,12 +24,12 @@ class BT {
         int[] arr;
         arr = new int[5];
         
-        // while (x < 20) {
-        //     a = b;
-        //     if (x < 1) 
-        //         x = x - 1;
-        //     b = false;
-        // }
+        while (x < 20) {
+            a = b;
+            // if (x < 1) 
+            //     x = x - 1;
+            // b = false;
+        }
         // while (x < 20) {
         //     a = b;
         //     if (x < 1) 
@@ -38,18 +38,18 @@ class BT {
         //         x = x + 1;
         //     b = false;
         // }
-        while (x < 1) {
-            a = b;  
-            while (x > 10) {
-                z = i;  
-            }
-        }
-        // if (x < 1) 
-        //     while (x < 1) {
-        //         a = b;  
+        // while (x < 1) {
+        //     a = b;  
+        //     while (x > 10) {
+        //         z = i;  
         //     }
-        // else 
-        //     x = x + 1;
+        // }
+        if (x < 1) 
+            while (x < 1) {
+                a = b;  
+            }
+        else 
+            x = x + 1;
         if (x < 1) 
             x = x - 1;
         else 
