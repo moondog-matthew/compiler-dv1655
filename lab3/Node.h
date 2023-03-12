0e9e28866eec7b8f886756b5dc8dc34de450d864
Marcus Kicklighter <marcus.kicklighter@gmail.com>
2023-03-12 19:35:35 +0100

    JumpBlocks for IfElse set

--- lab3/Node.h
+++ lab3/Node.h
@@ -477,14 +477,24 @@ public:
 		/* Assign the true and false path for the current block */
 		(*currentBlock)->setTrue(tBlock); 
 		(*currentBlock)->setFalse(fBlock);
-
+		
 		/*Fill fBlock and tBlock by calling genIR on various children. Called by reference*/
 		string tName = children[1]->genIR(&tBlock, methods, BBnames, id, blockID); // generate true block
 		string fName = children[2]->genIR(&fBlock, methods, BBnames, id, blockID); // generate false block
 		
+		// Add Tacs, unconditional and conditional jumps
+		/*Split is conditional*/
+		CondTac* splitTac = new CondTac(conName, fBlock->getName()); // default is to go to true
+		(*currentBlock)->add_Tac(splitTac);
+		/*True and false blocks have unconditional*/
+		JumpTac* trueTac = new JumpTac(jBlock->getName());
+		tBlock->add_Tac(trueTac);
+		JumpTac* falseTac = new JumpTac(jBlock->getName());
+		fBlock->add_Tac(falseTac);
+
 		// continue to write to the block after the while branch
 		*currentBlock = jBlock;
-		// TAC add conditional jump to jBlockName ifFalse ?? 
+		
 		return "";
 	}
 };
--- lab3/test_files/valid/MarcusIndiv.java
+++ lab3/test_files/valid/MarcusIndiv.java
@@ -50,7 +50,10 @@ class BT {
             // x = x - 1;
         // else 
         //     x = x + 1;
-
+        if (x < 1) 
+            x = x - 1;
+        else 
+            x = x + 1;
 
         x = x + 1;
 
